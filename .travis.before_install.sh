9e298e9d2fae3c6a944dcc6e8a2d69435e1bc775
Liam Minopulos <stunthacks@gmail.com>
2018-09-27 14:25:40 +0200

    Fix pacman packages

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -2,7 +2,7 @@
 set -e
 wget https://github.com/devkitPro/pacman/releases/download/devkitpro-pacman-1.0.1/devkitpro-pacman.deb
 sudo dpkg -i devkitpro-pacman.deb
-sudo dkp-pacman -S devkitARM picasso tex3ds 3dstools devkit-env libctru citro3d citro2d 3ds-zlib 3ds-tinyxml2 3ds-mpg123 3ds-libpng --noconfirm
+sudo dkp-pacman -S devkitARM picasso tex3ds 3dstools general-tools devkit-env libctru citro3d citro2d 3ds-zlib 3ds-tinyxml2 3ds-mpg123 3ds-libpng --noconfirm
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     wget https://github.com/StuntHacks/m3diaLib/releases/download/v0.1.2-beta/build-v0.1.2-beta.zip -O build.zip
     mkdir m3d-build
