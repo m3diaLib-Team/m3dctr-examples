25ea2582997a721d766bf139d24349b2e9847149
Liam Minopulos <stunthacks@gmail.com>
2018-09-27 13:56:15 +0200

    Add circlepad example

---
+++ input/circlepad/source/main.cpp
@@ -0,0 +1,27 @@
+#include <m3dia.hpp>
+
+void printAt(int t_x, int t_y, const std::string& t_text) {
+    printf("\x1b[%d;%dH%s\n", t_y, t_x, t_text.c_str());
+}
+
+int main() {
+    // initialize everything
+    m3d::Applet app;
+    m3d::Screen scr;
+    consoleInit(GFX_BOTTOM, NULL);
+
+    // main loop
+    while (app.isRunning()) {
+        // press B or Start to return to the homebrew menu
+        if (m3d::buttons::buttonPressed(m3d::buttons::Button::Start)) app.exit();
+        if (m3d::buttons::buttonPressed(m3d::buttons::Button::B)) app.exit();
+
+        printAt(0, 1, "Circlepad position: "
+                "{X: " + std::to_string(m3d::circlepad::getXPosition()) +
+                ", Y: " + std::to_string(m3d::circlepad::getYPosition()) + "}");
+
+        scr.render(); // render the screen
+    }
+
+    return 0;
+}
