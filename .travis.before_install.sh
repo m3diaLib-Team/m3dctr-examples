9e7f07f3f861b7510bb90d02e7d1c25327c78275
Liam Minopulos <stunthacks@gmail.com>
2018-10-15 09:50:48 +0200

    Add curl and mbedtls

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -2,7 +2,7 @@
 set -e
 wget https://github.com/devkitPro/pacman/releases/download/devkitpro-pacman-1.0.1/devkitpro-pacman.deb
 sudo dpkg -i devkitpro-pacman.deb
-sudo dkp-pacman -S devkitARM picasso tex3ds 3dstools general-tools devkit-env libctru citro3d citro2d 3ds-zlib 3ds-tinyxml2 3ds-mpg123 3ds-libpng --noconfirm
+sudo dkp-pacman -S devkitARM picasso tex3ds 3dstools general-tools devkit-env libctru citro3d citro2d 3ds-zlib 3ds-tinyxml2 3ds-mpg123 3ds-libpng 3ds-curl 3ds-mbedtls --noconfirm
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     wget https://github.com/StuntHacks/m3diaLib/releases/download/v0.2.1-beta/build-v0.2.1-beta.zip -O build.zip
     mkdir m3d-build
