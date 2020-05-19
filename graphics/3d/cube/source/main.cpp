c43c7bf5ba1e209e6c334ff9fadd9e972d900534
DaDamola <58088213+DaDamola@users.noreply.github.com>
2020-05-18 23:57:15 -0400

    Update main.cpp

--- graphics/3d/cube/source/main.cpp
+++ graphics/3d/cube/source/main.cpp
@@ -1,11 +1,17 @@
 #include <m3dia.hpp>
 
+using namespace m3d;
+
 int main() {
     // initialize everything
     m3d::Applet app;
     m3d::Screen scr;
     m3d::Texture tex;
-    m3d::Cuboid cube(0.7, 0.7, 0.7);
+    //m3d::Cuboid cube(0.7, 0.7, 0.7);
+	//Circle circle(4, 4, 10, Color(0, 0, 255));
+	//Rectangle rect(80, 80, 50, 75, Color(0, 255, 0));
+	//Polygon p(20, 20, 20);
+	
     consoleInit(GFX_BOTTOM, NULL);
 
     printf("Press A to toggle the texture");
@@ -14,11 +20,13 @@ int main() {
     tex.loadFromFile("romfs:/gfx/texture.png");
 
     // bind the texture
-    cube.bindTexture(tex);
+    //cube.bindTexture(tex);
 
+	
     // main loop
     while (app.isRunning()) {
         // press B to return to the homebrew menu
+		/*
         if (m3d::buttons::buttonPressed(m3d::buttons::Button::B)) app.exit();
 
         if (m3d::buttons::buttonPressed(m3d::buttons::Button::A)) {
@@ -30,12 +38,16 @@ int main() {
         }
 
         scr.drawTop(cube, m3d::RenderContext::Mode::Spatial); // draw the cube
+		*/
+		//scr.drawTop(rect, RenderContext::Mode::Flat);
         scr.render(); // render the screen
 
+		/*
         // rotate the cube each frame
         cube.rotatePitch(1);
         cube.rotateYaw(1);
         cube.rotateRoll(1);
+		*/
     }
 
     return 0;
